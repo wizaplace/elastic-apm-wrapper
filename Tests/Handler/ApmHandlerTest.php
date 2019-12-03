7e722639a18108d82c6fef30d963a07ffd9db37f
Eric COURTIAL <eric.courtial@wizacha.com>
2019-12-03 15:58:00 +0100

    Added PHPUnit bases

---
+++ Tests/Handler/ApmHandlerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @license     Proprietary
+ * @copyright   Copyright (c) Wizacha
+ */
+declare(strict_types=1);
+
+namespace Wizacha\ElasticApm\Tests\Handler;
+
+use PHPUnit\Framework\TestCase;
+use Wizacha\ElasticApm\Handler\ApmHandler;
+
+class ApmHandlerTest extends TestCase
+{
+
+}
---
+++ Tests/Service/AgentServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @license     Proprietary
+ * @copyright   Copyright (c) Wizacha
+ */
+declare(strict_types=1);
+
+namespace Wizacha\ElasticApm\Tests\Handler;
+
+use PHPUnit\Framework\TestCase;
+use Wizacha\ElasticApm\Service\AgentService;
+
+class AgentServiceTest extends TestCase
+{
+    public function test_start_transaction() : void
+    {
+        $this->fail('SORRY');
+    }
+}
